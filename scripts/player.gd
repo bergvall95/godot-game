7dc69c44a0b7439ead33d307d8c148a030d4133d
alex bergvall <bergvall95@gmail.com>
2024-06-17 18:34:55 -0600

    first commit

---
+++ scripts/player.gd
@@ -0,0 +1,85 @@
+extends CharacterBody3D
+
+# movement vals
+var speed 
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.003
+const AIR_CONTROL_MOD = 1.0
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 9.8
+var direction = 0.0
+
+# player experience vals
+const BOB_FREQ = 2.0
+const BOB_AMP= 0.03
+var t_bob= 0.0
+
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
+
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+@onready var head = $head
+@onready var camera = $head/playercam
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90));
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	
+
+	direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	# handle sprint
+	if Input.is_action_pressed('sprint'):
+		speed = SPRINT_SPEED
+	else: 
+		speed = WALK_SPEED
+		
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.x * speed, delta * 7.0)
+	else: 
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0)
+		velocity.z = lerp(velocity.z, direction.x * speed, delta * 2.0)
+	# head bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
+	# FOV 
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+	
+	move_and_slide()
+	
+	
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ) * BOB_AMP
+	return pos
+		
+		
