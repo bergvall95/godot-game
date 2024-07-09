7dd5381a7b965504b29000119410d30e856f9463
alex bergvall <bergvall95@gmail.com>
2024-07-08 22:57:00 -0600

    movement done

--- Scenes/zombie.gd
+++ Scenes/zombie.gd
@@ -26,7 +26,7 @@ func _process(delta):
 			nav_agent.set_target_position(player.global_transform.origin)
 			var next_nav_point = nav_agent.get_next_path_position()
 			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-			look_at(Vector3(global_position.x + velocity.x, global_position.y, global_position.z + velocity.z ), Vector3.UP)
+			rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), delta*10 )
 		"Attack":
 			look_at(Vector3(player.global_position.x, player.global_position.y, player.global_position.z ), Vector3.UP)
 		
